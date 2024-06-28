e573f617b971a16e1ffa93ef6a177847bf862851
ThayMendes <thay.gm@live.com>
2024-06-27 23:30:07 -0300

    contador+remove+ chave api

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
+let favoriteVideos = [];
+
 function searchVideos() {
-    const apiKey = 'AIzaSyCFAX8eOotr4xTHEO9wZ2bLGT1hHsWftTA';
+    const apiKey = 'Adicione sua chave api aqui';
     const searchTerm = document.getElementById('search').value;
     const maxResults = 12;
 
@@ -26,6 +28,7 @@ function displayVideos(videos) {
         const videoId = video.id.videoId;
         const videoTitle = video.snippet.title;
         const videoThumbnail = video.snippet.thumbnails.medium.url;
+        const isFavorite = favoriteVideos.includes(videoTitle);
         const videoElement = document.createElement('div');
         videoElement.classList.add('video');
 
@@ -33,6 +36,7 @@ function displayVideos(videos) {
             <iframe width="300" height="200" src="https://www.youtube.com/embed/${videoId}" 
                 title="${videoTitle}" frameborder="0" allowfullscreen></iframe>
             <h6>${videoTitle}</h6>
+            <i class="${isFavorite ? 'bi bi-star-fill' : 'bi bi-star'} favorite-icon" onclick="toggleFavorite(this)"></i>
         `;
 
         videoContainer.appendChild(videoElement);
@@ -44,5 +48,47 @@ function displayVideos(videos) {
         if (titleWidth > videoWidth) {
             titleElement.classList.add('long-title');
         }
+    });
+}
+
+//para adicionar/remover favoritos
+function toggleFavorite(icon) {
+    icon.classList.toggle('bi-star');
+    icon.classList.toggle('bi-star-fill');
+    const videoTitle = icon.previousElementSibling.textContent;
+
+    if (icon.classList.contains('bi-star-fill')) {
+        favoriteVideos.push(videoTitle);
+    } else {
+        const index = favoriteVideos.indexOf(videoTitle);
+        if (index !== -1) {
+            favoriteVideos.splice(index, 1);
+        }
+    }
+
+    updateFavoriteCount();
+    updateFavoriteVideos();
+}
+
+//para atualizar o contador de favoritos
+function updateFavoriteCount() {
+    const favoriteCountElement = document.getElementById('favorite-count');
+    favoriteCountElement.textContent = favoriteVideos.length;
+}
+
+//para atualizar a lista de favoritos na página favoritos
+function updateFavoriteVideos() {
+    const favoriteVideosContainer = document.getElementById('favorite-videos');
+    favoriteVideosContainer.innerHTML = '';
+
+    favoriteVideos.forEach(videoTitle => {
+        const videoElement = document.createElement('div');
+        videoElement.classList.add('video');
+
+        videoElement.innerHTML = `
+            <h6>${videoTitle}</h6>
+        `;
+
+        favoriteVideosContainer.appendChild(videoElement);
     });
 }
